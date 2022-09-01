0aaeb28ce41ae807f9ee2eb03e61164e3b279206
Mustafa Abdulrahman <mus2003.abdul@gmail.com>
2022-09-01 00:19:48 -0400

    ran prettier on js file

--- hackathon_site/event/static/event/js/application.js
+++ hackathon_site/event/static/event/js/application.js
@@ -1,3 +1,5 @@
 $(document).ready(function () {
-    $("#id_why_participate, #id_what_technical_experience, #id_what_past_experience").characterCounter();
+    $(
+        "#id_why_participate, #id_what_technical_experience, #id_what_past_experience"
+    ).characterCounter();
 });
