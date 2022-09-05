3f941575f8243fc56fd40d1fb74517fde55dbda0
YuYing Liang <yuying.liang00@gmail.com>
2022-09-04 23:35:56 -0400

    formatted files

--- hackathon_site/dashboard/frontend/src/components/dashboard/EditTeam/EditTeam.tsx
+++ hackathon_site/dashboard/frontend/src/components/dashboard/EditTeam/EditTeam.tsx
@@ -89,7 +89,8 @@ export const EditTeam = ({ teamCode, canChangeTeam, teamSize }: TeamModalProps) 
             <div className={styles.editTeamModal}>
                 <div className={styles.editTeamModalDiv}>
                     <Typography variant="body1">
-                        Create a team with up to 4 people. Share your code with others or join another team.
+                        Create a team with up to 4 people. Share your code with others
+                        or join another team.
                     </Typography>
                     <Typography variant="body2" className={styles.heading}>
                         Note: You do not have to stay in the same team you applied with.
