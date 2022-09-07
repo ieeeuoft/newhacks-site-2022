5b3f9c2edb4b5953f162ad9df5b99a84f3af0686
YuYing Liang <yuying.liang00@gmail.com>
2022-09-07 16:55:45 -0400

    added base url to whitelist

--- hackathon_site/hackathon_site/settings/__init__.py
+++ hackathon_site/hackathon_site/settings/__init__.py
@@ -44,7 +44,8 @@ if DEBUG:
 else:
     ALLOWED_HOSTS = ["newhacks.ca", "www.newhacks.ca", "hardware.newhacks.ca"]
     CORS_ORIGIN_REGEX_WHITELIST = [
-        r"^https://(?:www\.)?newhacks.ca(/\w+)+"
+        r"^https://(?:www\.)?newhacks.ca(/\w+)+",
+        r"^https://(?:www\.)?hardware.newhacks.ca",
         r"^https://(?:www\.)?hardware.newhacks.ca(/\w+)+",
     ]
 
