7f3a7634602236c0ed6b9f84232e1395efbb23ca
YuYing Liang <yuying.liang00@gmail.com>
2022-09-24 15:59:46 -0400

    updated hss start/end dates

--- hackathon_site/dashboard/frontend/src/constants.js
+++ hackathon_site/dashboard/frontend/src/constants.js
@@ -2,5 +2,5 @@ export const hackathonName = "NewHacks";
 export const adminGroup = "Hardware Site Admins";
 export const minTeamSize = 3;
 export const maxTeamSize = 4;
-export const hardwareSignOutStartDate = new Date(2022, 9, 21, 0);
-export const hardwareSignOutEndDate = new Date(2022, 10, 6, 17);
+export const hardwareSignOutStartDate = new Date(2022, 10, 21, 0);
+export const hardwareSignOutEndDate = new Date(2022, 11, 6, 17);
