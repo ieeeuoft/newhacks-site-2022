ec5351e8cf061d881399cfb073ac6dcf50947718
YuYing_Liang <yuying.liang00@gmail.com>
2022-10-23 20:11:11 -0400

    updated discord and participant package links (#28)

--- hackathon_site/hackathon_site/settings/__init__.py
+++ hackathon_site/hackathon_site/settings/__init__.py
@@ -338,11 +338,11 @@ WAITLISTED_ACCEPTANCE_START_TIME = EVENT_START_DATE + timedelta(hours=1)
 FINAL_REVIEW_RESPONSE_DATE = REGISTRATION_CLOSE_DATE + timedelta(days=7)
 
 # Links
-PARTICIPANT_PACKAGE_LINK = "#"
+PARTICIPANT_PACKAGE_LINK = "https://docs.google.com/document/d/1JvPlvxwMze9dqjv_5PIUVu8bHTiEZz8AtviHRMEsWQQ/edit?usp=sharing"
 
 # Note this is in the form (chat_room_name, chat_room_link)
 # Chat room name is such as the following: Slack, Discord
-CHAT_ROOM = ("Discord", "https://discord.com")
+CHAT_ROOM = ("Discord", "https://discord.gg/BQg4Upq3pm")
 
 # HSS Testing
 TEST_USER_GROUP = "HSS Test Users"
